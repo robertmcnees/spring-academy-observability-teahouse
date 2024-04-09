58e0011de9f58e3e5384c6f30c1f2af2f7e72ae1
Robert McNees <rmcnees@vmware.com>
2024-04-09 08:54:32 -0400

    Remove unused code related to an unused method of loading in the lab

--- scripts/drop-create-database.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# This script is used to drop and recreate the meta-data tables
-
-docker exec postgres psql -f /mnt/exercises/sql/schema-drop-postgresql.sql -U postgres
-
-docker exec postgres psql -f /mnt/exercises/sql/schema-water-service.sql -U postgres
-docker exec postgres psql -f /mnt/exercises/sql/insert-water-service.sql -U postgres
